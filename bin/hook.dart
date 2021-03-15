984a561e9b67da8d9aecbbc2c7dd7b6cec660044
xuzhongpeng <1452754335@qq.com>
2021-03-15 21:19:10 +0800

    fix: format and add hook.dart

--- bin/hook.dart
+++ bin/hook.dart
@@ -1,31 +1,28 @@
-import "package:git_hooks/git_hooks.dart";
-// import "dart:io";
+import 'package:git_hooks/git_hooks.dart';
+import 'dart:io';
 
 void main(List arguments) {
-  Map<Git, UserBackFun> params = {
-    Git.commitMsg: commitMsg,
-    Git.preCommit: preCommit
-  };
+  var params = {Git.commitMsg: commitMsg, Git.preCommit: preCommit};
   GitHooks.call(arguments, params);
 }
 
 Future<bool> commitMsg() async {
-  // String rootDir = Directory.current.path;
-  // String commitMsg = Utils.getCommitEditMsg();
-  // if (commitMsg.startsWith('fix:')) {
-  //   return true; // you can return true let commit go
-  // } else
-  //   return false;
-  return true;
+  var commitMsg = Utils.getCommitEditMsg();
+  if (commitMsg.startsWith('fix:')) {
+    return true; // you can return true let commit go
+  } else {
+    return false;
+  }
 }
 
 Future<bool> preCommit() async {
-  // try {
-  //   ProcessResult result = await Process.run('dartanalyzer', ['bin']);
-  //   print(result.stdout);
-  //   if (result.exitCode != 0) return false;
-  // } catch (e) {
-  //   return false;
-  // }
-  return true;
+  try {
+    var result = await Process.run('dartanalyzer', ['bin']);
+    print(result.stdout);
+    print(result.stderr);
+    if (result.exitCode != 0) return false;
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,7 @@
 import 'package:git_hooks/git_hooks.dart';
 
-void main() async{
-  GitHooks.init(targetPath: "bin/git_hooks.dart");
+void main() async {
+  GitHooks.init(targetPath: 'bin/hook.dart');
   // print(await GitHooks.getTargetFilePath());
   // GitHooks.unInstall();
 }
