ef5aa591b8fe29950a9cd80103eea095047464ac
xuzhongpeng <1452754335@qq.com>
2022-12-12 22:57:50 +0800

    add null safety

--- bin/hook.dart
+++ bin/hook.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 
 void main(List arguments) {
   var params = {Git.commitMsg: commitMsg, Git.preCommit: preCommit};
-  GitHooks.call(arguments, params);
+  GitHooks.call(arguments as List<String>, params);
 }
 
 Future<bool> commitMsg() async {
   var commitMsg = Utils.getCommitEditMsg();
-  if (commitMsg.startsWith('fix:')) {
+  if (commitMsg.startsWith('fix:') || commitMsg.startsWith('feat:')) {
     return true; // you can return true let commit go
   } else {
     return false;
@@ -17,10 +17,10 @@ Future<bool> commitMsg() async {
 
 Future<bool> preCommit() async {
   try {
-    var result = await Process.run('dartanalyzer', ['bin']);
-    print(result.stdout);
-    print(result.stderr);
-    if (result.exitCode != 0) return false;
+    // var result = await Process.run('dart', ['analyze']);
+    // print(result.stdout);
+    // print(result.stderr);
+    // if (result.exitCode != 0) return false;
     return true;
   } catch (e) {
     return false;
